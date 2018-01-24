46a715c847010da622c0f4437c795d362a86dd14
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-01-24 14:23:06 -0600

    pictures rendering from flickr

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 console.log("this worked")
 $.get({
-  url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3ec63ca541cfee22909340679072dec9&tags=puppy&format=json&nojsoncallback=1&auth_token=72157689681998012-fb6c37bf4a5a52ec&api_sig=116044bf7f0df75e7d45fbf125d0dbb0",
+  url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=866c98193c22567ad21e0f5bffc68f9a&tags=puppy&format=json&nojsoncallback=1&auth_token=72157691854939594-7a933e6483932837&api_sig=e4cfce3d2eed60a6e464a6020dbe59b1",
 })
 .done(function(res) {
-  console.log(res)
+  console.log(res.photos.photo)
+  res.photos.photo.forEach(function(pic) {
+    //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    $("#list").append('<img src="https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg" > </img>')
+  })
 })
