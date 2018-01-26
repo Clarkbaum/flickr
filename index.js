aa0b8795e43029900c61f13d7cbb2c36e6ac8e78
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-01-26 13:11:59 -0600

    title added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-console.log("this worked")
 $.get({
-  url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=866c98193c22567ad21e0f5bffc68f9a&tags=puppy&format=json&nojsoncallback=1&auth_token=72157691854939594-7a933e6483932837&api_sig=e4cfce3d2eed60a6e464a6020dbe59b1",
+  url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f399a174c74917573c75f080c96162ac&tags=puppy&format=json&nojsoncallback=1&auth_token=72157692669015065-41ea4fed6958bf83&api_sig=07dfdfe1c7c7095f269bae6087777bb2",
 })
 .done(function(res) {
   console.log(res.photos.photo)
   res.photos.photo.forEach(function(pic) {
     //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-    $("#list").append('<img src="https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg" > </img>')
+    $("#list")
+      .append('<div> ' + pic.title + ' </div>')
+      .append('<img src="https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg" > </img>')
+
   })
 })
