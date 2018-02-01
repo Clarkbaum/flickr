c51aa8d9ee7df07f7c3a39fdcd2a5c4de96fafd3
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-02-01 14:10:50 -0600

    you can now choose what to search for instead of defaulting to puppy everytime

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-$.get({
-  url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f5ab29a2e940fc1280543f6206e186db&tags=puppy&format=json&nojsoncallback=1",
-})
-.done(function(res) {
-  console.log(res.photos.photo)
-  res.photos.photo.forEach(function(pic) {
-    //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-    $("#list")
-      .append('<div> ' + pic.title + ' </div>')
-      .append('<img src="https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg" > </img>')
-
+function getFlickr(tags = 'puppy') {
+  $.get({
+    url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f5ab29a2e940fc1280543f6206e186db&tags="+ tags +"&format=json&nojsoncallback=1",
   })
-})
-.catch(function() {
-  console.log("error hitting endpoint")
+  .done(function(res) {
+    console.log(res.photos.photo)
+    $("#list").empty();
+    res.photos.photo.forEach(function(pic) {
+      //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+      $("#list")
+        .append('<div> ' + pic.title + ' </div>')
+        .append('<img src="https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg" > </img>')
+
+    })
+  })
+  .catch(function() {
+    console.log("error hitting endpoint")
+  })
+}
+
+getFlickr();
+
+$('#search').on("keyup", function(e) {
+  if(e.key === "Enter"){
+    console.log("this", $(this).val())
+    getFlickr($(this).val());
+  }
 })
